872a9397ddb4b470b57359aa1876b8052d5cd7a0
Samuel Knox <sam@evorlor.com>
2016-03-07 11:17:13 -0500

    improved rigidbody2d extensions for bad gravity input

--- Assets/Custom Unity Library/2D/Extensions/UnityEngine/Rigidbody2DExtensions.cs
+++ Assets/Custom Unity Library/2D/Extensions/UnityEngine/Rigidbody2DExtensions.cs
@@ -5,6 +5,8 @@
 /// </summary>
 public static class Rigidbody2DExtensions
 {
+    private const float DefaultGravity = -9.8f;
+
     /// <summary>
     /// Applies a force to the Rigidbody2D such that it will land, if unobstructed, at the target position.
     /// The arch [0.0f, 1.0f] determines the percent of arch to provide between the minimum and maximum arch.
@@ -13,7 +15,7 @@ public static class Rigidbody2DExtensions
     /// If target is out of range, it will still fire, but not reach the target.
     /// Use rigidbody2D.IsWithinRange(Vector2, float) to determine if the target is within range ahead of time, if destired.
     /// This only takes the Y gravity into account, and X gravity will not affect the trajectory.
-    /// If there is no gravity, a fixed gravity of (0.0f, -9.8f) will be applied.
+    /// If there is no gravity and no ConstantForce2D is attached, a ConstantForce2D with a gravity of (0.0f, -9.8f) will be attached.
     /// </summary>
     public static bool SetTrajectory(this Rigidbody2D rigidbody2D, Vector2 target, float force, float arch = 0.5f)
     {
@@ -24,8 +26,14 @@ public static class Rigidbody2DExtensions
         float gravity = -Physics2D.gravity.y;
         if (gravity == 0)
         {
-            var fixedGravity = rigidbody2D.gameObject.AddComponent<FixedGravity2D>();
-            gravity = -fixedGravity.GetGravity().y;
+            var constantForce2D = rigidbody2D.GetComponent<ConstantForce2D>();
+            if (!constantForce2D)
+            {
+                Debug.LogWarning("There is no gravity and " + rigidbody2D.name + " does not have a ConstantForce2D attached.  A ConstantForce2D with the default gravity of -9.8f will be added.");
+                constantForce2D = rigidbody2D.gameObject.AddComponent<ConstantForce2D>();
+                constantForce2D.force = new Vector2(0, DefaultGravity);
+            }
+            gravity = -constantForce2D.force.y;
         }
         float b = force * force - y * gravity;
         float discriminant = b * b - gravity * gravity * (x * x + y * y);
@@ -44,79 +52,30 @@ public static class Rigidbody2DExtensions
 
     /// <summary>
     /// Checks if the Rigidbody2D is within range such that if rigidbody2D.SetTrajectory(Vector2, float) is called on it, it will be able to reach its target.
+    /// If there is no gravity and no ConstantForce2D is attached, a default gravity of -9.8f will be assumed.
     /// </summary>
     public static bool IsWithinRange(this Rigidbody2D rigidbody2D, Vector2 target, float force, float arch = 0.5f)
     {
-        Mathf.Clamp(arch, 0, 1);
+        arch = Mathf.Clamp(arch, 0, 1);
         var origin = rigidbody2D.position;
         float x = target.x - origin.x;
         float y = target.y - origin.y;
         float gravity = -Physics2D.gravity.y;
         if (gravity == 0)
         {
-            var temporaryGameObject = new GameObject();
-            var temporaryFixedGravity = temporaryGameObject.AddComponent<FixedGravity2D>();
-            gravity = -temporaryFixedGravity.GetGravity().y;
-            Object.Destroy(temporaryGameObject);
+            var constantForce2D = rigidbody2D.GetComponent<ConstantForce2D>();
+            if (!constantForce2D)
+            {
+                Debug.LogWarning("There is no gravity and " + rigidbody2D.name + " does not have a ConstantForce2D attached.  The default gravity of -9.8f will be assumed.");
+                gravity = -DefaultGravity;
+            }
+            else
+            {
+                gravity = -constantForce2D.force.y;
+            }
         }
         float b = force * force - y * gravity;
         float discriminant = b * b - gravity * gravity * (x * x + y * y);
         return discriminant >= 0;
-    }
-}
-
-[RequireComponent(typeof(Rigidbody2D))]
-///Applies gravity to a single Rigidbody2D, optionally ignoring the project-wide gravity
-public class FixedGravity2D : MonoBehaviour
-{
-    [Tooltip("The fixed gravity to be applied to the Rigidbody2D")]
-    [SerializeField]
-    private Vector2 gravity = new Vector2(0, -9.8f);
-
-    [Tooltip("Whether or not to ignore the project's gravity")]
-    [SerializeField]
-    private bool ignoreProjectGravity = true;
-
-    private Rigidbody2D body2D;
-    private float gravityScale;
-
-    void Awake()
-    {
-        body2D = GetComponent<Rigidbody2D>();
-        gravityScale = body2D.gravityScale;
-    }
-
-    void FixedUpdate()
-    {
-        body2D.AddForce(gravity * body2D.mass);
-    }
-
-    void OnValidate()
-    {
-        if (ignoreProjectGravity)
-        {
-            body2D.gravityScale = 0;
-        }
-        else
-        {
-            body2D.gravityScale = gravityScale;
-        }
-    }
-
-    /// <summary>
-    /// Gets the fixed gravity applied to the Rigidbody2D
-    /// </summary>
-    public Vector2 GetGravity()
-    {
-        return gravity;
-    }
-
-
-    /// <summary>
-    /// Sets the fixed gravity applied to the Rigidbody2D
-    /// </summary>
-    public void SetGravity(Vector2 gravity)
-    {
-        this.gravity = gravity;
     }
 }
